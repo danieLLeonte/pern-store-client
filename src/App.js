3c704501a36cb65312bf458ed71a487c573af9f3
Daniel Leonte <leontedaniel37@gmail.com>
2023-11-14 15:27:22 +0200

    refactor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import axios from "axios";
 
 import { Footer, Header } from "./components";
 import { Home } from "./pages";
+
+axios.defaults.baseURL =
+  process.env.REACT_APP_SERVER_URL || "http://localhost:3001";
 
 function App() {
   return (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     useEffect(() => {
         const getProducts = async () => {
             try {
-                const products = await axios.get("http://localhost:3001/api/products");
+                const products = await axios.get("/api/products");
                 console.log(products)
                 setProducts(products.data);
             } catch (error) {
@@ -21,11 +21,10 @@ const Home = () => {
         <div className="grid grid-cols-4">
             {products.map(product => (
                 <div key={product.id} className="bg-gray-300 rounded-md p-2 m-2 flex flex-col gap-2">
-                    <img src="/logo192.png" alt={product.name} className="object-cover" />
+                    <img src={product.imageurl} alt={product.name} className="object-cover" />
                     <p>{product.name}</p>
-                    {/* <p>{product.description}</p> */}
                     <p>{product.price}</p>
-                    {/* <p>{product.quantity}</p> */}
+                    <button className="p-2 rounded-md bg-gray-400">Add to Cart</button>
                 </div>
             ))}
         </div>
